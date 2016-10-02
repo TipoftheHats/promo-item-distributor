d538072a33e7884e839a08bdc7feed286bd5f60b
Alex Van Camp <vancamp.alex@gmail.com>
2016-10-02 09:44:57 -0500

    add file logging via Winston

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const TABLE_NAME = 'promo_records_2016';
 const fs = require('fs');
 const pg = require('pg');
 const request = require('sync-request');
+const winston = require('winston');
 const convict = require('convict');
+
+// Set up logging. Every run of this program logs to a new file.
+if (!fs.existsSync('logs')) {
+	fs.mkdirSync('logs');
+}
+winston.add(winston.transports.File, {
+	level: 'silly',
+	filename: `logs/${Date.now()}.log`
+});
+
+// Set up configuration
 const conf = convict({
 	env: {
 		doc: 'The applicaton environment.',
@@ -30,7 +42,7 @@ const conf = convict({
 }).getProperties();
 
 if (!conf.password) {
-	console.error('No password provided!');
+	winston.log('error', 'No password provided!');
 	process.exit(1);
 }
 
@@ -43,18 +55,18 @@ const pgConfig = {
 };
 
 if (conf.env === 'production') {
-	// console.warn(`Running in PRODUCTION against a LIVE DATABASE at ${conf.host}!`);
-	console.log('Running in production mode is currently disabled.');
+	// winston.log('warn', `Running in PRODUCTION against a LIVE DATABASE at ${conf.host}!`);
+	winston.log('info', 'Running in production mode is currently disabled.');
 	process.exit(0);
 	pgConfig.host = conf.host;
 } else {
-	console.info('Running in development mode against localhost.');
+	winston.log('info', 'Running in development mode against localhost.');
 }
 const pgClient = new pg.Client(pgConfig);
 const recipients = require('./recipients.json');
 
 pgClient.on('error', e => {
-	console.error('Pool error!', e);
+	winston.log('error', 'Pool error!', e);
 });
 
 // Connect to tracker-database.tipofthehats.org
@@ -62,30 +74,30 @@ pgClient.connect();
 pgClient.query(`SELECT to_regclass('${TABLE_NAME}');`)
 	.then(result => {
 		if (result.rows[0].to_regclass === null) {
-			console.log(`Table ${TABLE_NAME} not found, creating...`);
+			winston.log('info', `Table ${TABLE_NAME} not found, creating...`);
 			const createTableSQL = fs.readFileSync('create_table.sql', 'utf-8');
 			return pgClient.query(createTableSQL);
 		}
 
-		console.log(`Table ${TABLE_NAME} found!`);
+		winston.log('info', `Table ${TABLE_NAME} found!`);
 	})
 	.then(result => {
 		if (result && result.command === 'CREATE') {
-			console.log(`Successfully created table ${TABLE_NAME}!`)
+			winston.log('info', `Successfully created table ${TABLE_NAME}!`);
 		}
 
 		// Load all recipients into the database. We'll give out the medals later.
 		recipients.forEach(recipient => {
 			pgClient.query(`INSERT INTO promo_records_2016 (steamid64) VALUES ('${recipient.steamid64}') ON CONFLICT (steamid64) DO NOTHING;`, (err, result)=> {
 				if (err) {
-					console.error(`Error adding SteamID64 "${recipient.steamid64}" to database:`, err);
+					winston.log('error', `Error adding SteamID64 "${recipient.steamid64}" to database:`, err);
 					return;
 				}
 
 				if (result.rowCount > 0) {
-					console.log(`Added SteamID64 "${recipient.steamid64}" to database.`);
+					winston.log('debug', `Added SteamID64 "${recipient.steamid64}" to database.`);
 				} else {
-					console.log(`Skipped adding SteamID64 "${recipient.steamid64}" to database, already present`);
+					winston.log('debug', `Skipped adding SteamID64 "${recipient.steamid64}" to database, already present`);
 				}
 			});
 		});
@@ -94,7 +106,7 @@ pgClient.query(`SELECT to_regclass('${TABLE_NAME}');`)
 			// This seems to fire one tick too early, so we use a nextTick in here.
 			pgClient.once('drain', () => {
 				process.nextTick(() => {
-					console.log('All donors added to database.');
+					winston.log('info', 'All donors added to database.');
 					resolve();
 				});
 			});
@@ -120,7 +132,7 @@ recipients.forEach(recipient => {
 
 	const result = JSON.parse(res.getBody('UTF-8')).result;
 	if (result.status === 1) {
-		console.log('Awarded Jaunty Pin (ID #%s) to %s', result.item_id, id);
+		winston.log('info', 'Awarded Jaunty Pin (ID #%s) to %s', result.item_id, id);
 	} else {
 		if (result.status === 2 && result.statusDetail.contains('Unable to load/lock account')) {
 			// This means the person put in a bad account and we can't do anything about it
