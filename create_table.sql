861e1fd7bf7574430f761a9bd5ef4738c06bdad0
Alex Van Camp <vancamp.alex@gmail.com>
2018-10-20 12:25:42 -0500

    feat: update year const from 2017 to 2018

--- create_table.sql
+++ create_table.sql
@@ -1,29 +1,29 @@
-CREATE TABLE IF NOT EXISTS "2017_donors" (
+CREATE TABLE IF NOT EXISTS "2018_donors" (
     steamid64 character varying(32) NOT NULL,
     promo_item_awarded character varying(16) DEFAULT '',
-    CONSTRAINT "2017_donors_pkey" PRIMARY KEY (steamid64)
+    CONSTRAINT "2018_donors_pkey" PRIMARY KEY (steamid64)
 );
 
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = '2017_donation_type') THEN
-       CREATE TYPE "2017_donation_type" AS ENUM('item', 'cash');
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = '2018_donation_type') THEN
+       CREATE TYPE "2018_donation_type" AS ENUM('item', 'cash');
     END IF;
 END$$;
 
-CREATE TABLE IF NOT EXISTS "2017_donations" (
+CREATE TABLE IF NOT EXISTS "2018_donations" (
     id character varying(32) NOT NULL,
     steamid64 character varying(32) NOT NULL,
     email character varying(128),
-    type "2017_donation_type",
+    type "2018_donation_type",
     amount NUMERIC(16, 2),
-    CONSTRAINT "2017_donations_pkey" PRIMARY KEY (id)
+    CONSTRAINT "2018_donations_pkey" PRIMARY KEY (id)
 );
 
-CREATE FUNCTION total_donated("2017_donors")
+CREATE FUNCTION total_donated("2018_donors")
   RETURNS NUMERIC(16, 2) AS
 $func$
     SELECT sum(amount)
-    FROM   "2017_donations"
-    WHERE  "2017_donations".steamid64 = $1.steamid64
+    FROM   "2018_donations"
+    WHERE  "2018_donations".steamid64 = $1.steamid64
 $func$ LANGUAGE SQL STABLE;
